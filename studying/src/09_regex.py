fe136149418f520d6bd91705f3f79e2218ff459a
ww5365 <ww5365@163.com>
2021-04-17 14:42:06 +0800

    test

--- studying/src/09_regex.py
+++ studying/src/09_regex.py
@@ -9,28 +9,103 @@ re： 正则表达式模块的使用
 https://www.runoob.com/python3/python3-reg-expressions.html
 
 
+特殊字符类：
+.	匹配除 "\n" 之外的任何单个字符。要匹配包括 '\n' 在内的任何字符，请使用象 '[.\n]' 的模式。
+\d	匹配一个数字字符。等价于 [0-9]。
+\D	匹配一个非数字字符。等价于 [^0-9]。
+\s	匹配任何空白字符，包括空格、制表符、换页符等等。等价于 [ \f\n\r\t\v]。
+\S	匹配任何非空白字符。等价于 [^ \f\n\r\t\v]。
+\w	匹配包括下划线的任何单词字符。等价于'[A-Za-z0-9_]'。
+\W	匹配任何非单词字符。等价于 '[^A-Za-z0-9_]'。
+
 正则表达式[\w]+,\w+,[\w+] 三者有何区别：
     [\w]+和\w+没有区别，都是匹配数字和字母下划线的多个字符；
     [\w+]表示匹配数字、字母、下划线和加号本身字符；
-    
 
-星号 (*) 匹配前面的字符 0 次或多次。
+匹配字符个数：
+    用*表示任意个字符（包括0个）
+    用+表示至少一个字符
+    用?表示0个或1个字符
+    用{n}表示n个字符
+    用{n,m}表示n-m个字符：
+    星号 (*) 匹配前面的字符 0 次或多次。
+    .* : 匹配任意字符
+
     例如，10* 可以匹配：
     1
     10
     100
     1000
-    
-.* : 匹配任意字符
 
-问号 (?) 匹配前面的字符 0 次或 1 次。
-    例如，10? 可以匹配：
-    1
-    10
-    
+
+    问号 (?) 匹配前面的字符 0 次或 1 次。
+        例如，10? 可以匹配：
+        1
+        10
+
+分组：()
 (abc|bcd|cde)，表示这一段是abc、bcd、cde三者之一均可，顺序也必须一致
 
 '''
+
+
+def test_re_match():
+    '''
+    re.match(pattern, string, flags=0):
+
+    功能：从字符串的起始位置匹配一个模式，如果不是起始位置匹配成功的话，match()就返回None
+    参数说明：
+    flags: re.I 大小写不敏感  re.M 多行匹配   多个标识使用： |  来生效
+    输出：
+    匹配对象或None
+    group(num = 0):
+      匹配的整个表达式的字符串，group() 可以输入多个组号，它返回包含那些组所对应值的元组
+
+    groups():
+        返回一个包含所有小组字符串的元组，从 1 到 所含的小组号
+
+    另外：span函数用来返回匹配对象起始位置和结束位置
+
+    '''
+
+    print(re.match('wwW', 'www.baidu.com www.huawei.com',
+                   re.I).span())  # re.I 忽略大小写；span（）返回[begin, end) 结束位置 (0, 3)
+
+    print(re.match('wwW', 'www.baidu.com www.huawei.com',
+                   re.I).groups())  # groups(): 空元祖，因为是从1开始的
+
+    print(re.match('bai', 'www.baidu.com', re.I))  # 不在起始位置匹配，返回None
+
+    line = 'Cats are smarter than dogs'
+
+    #  .* 匹配任意字符，至少0个字符 () 是1个匹配组  .+ 匹配任意字符，至少1个字符
+    # (.*?) 是非贪婪匹配,加上? 以最少的可能性匹配 默认是贪婪方式，尽可能多的可能来匹配
+    match_obj = re.match(r'(.*) are (.+?)(.+)', line, re.I)
+
+    if match_obj:
+        print(match_obj.groups())  # 返回一个包含所有小组字符串的元组
+        print(match_obj.group())  # 匹配的整个表达式的字符串结果
+        print(match_obj.group(1))  # 从1开始，匹配对象中的索引
+        print(match_obj.group(2))
+        # print(match_obj.group(3))
+    else:
+        print("match result is None!")
+
+
+def test_re_search():
+    '''
+    re.search(pattern, string, flags = 0)
+
+    输入和输出：类似re.match
+
+    区别： 可以在任意位置开始匹配，匹配到就返回匹配对象，否则None
+
+    '''
+    print(re.search('BAI', 'www.baidu.com', re.I).span())  # 从bai开始匹配成功
+    str1 = 'A23G4HFD567'
+    # (?P<name> Expression) 命名捕获组 可以通过name来使用匹配组的值
+    str_match = re.search(r'(?P<value>\d+)', str1)
+    print("re.search :  ", str_match.group('value'))
 
 
 def test_re_sub():
@@ -65,12 +140,9 @@ def test_re_sub():
     line = re.sub(r"#E\-[\w]*\[(得意|傲慢)+\]", "α", line)
     print("sub:", line)
 
-    key = "zh_hk_alias"
-    key = re.sub(r'_alias\d*', '', key)
-    print(key)
-
 
 def string_filter(input_str: str):
+    # 使用[]限定过滤集合
     pat = r"[`~!@#$%^()={}:;\[\]<>《》/！￥…（）【】‘；：”“\"’。、?·,，]"
     if len(input_str) <= 0:
         return input_str
@@ -86,64 +158,20 @@ def string_filter(input_str: str):
 
 
 if __name__ == '__main__':
-    '''
-    re.match(pattern, string, flags=0): 
 
-    功能：从字符串的起始位置匹配一个模式，如果不是起始位置匹配成功的话，match()就返回None
-    参数说明：
-    flags: re.I 大小写不敏感  re.M 多行匹配   多个标识使用： |  来生效
-    输出：
-    匹配对象或None
-    group(num = 0):
-      匹配的整个表达式的字符串，group() 可以输入多个组号，它返回包含那些组所对应值的元组
-
-    groups():  
-        返回一个包含所有小组字符串的元组，从 1 到 所含的小组号
-
-    另外：span函数用来返回匹配对象起始位置和结束位置 
-
-    '''
-
-    print(re.match('wwW', 'www.baidu.com www.huawei.com',
-                   re.I).span())  # 返回[begin, end) 结束位置 (0, 3)
-    print(re.match('bai', 'www.baidu.com', re.I))  # 不在起始位置匹配，返回None
-
-    line = 'Cats are smarter than dogs'
-
-    #  .* 匹配任意字符，至少0个字符 () 是1个匹配组  .+ 匹配任意字符，至少1个字符
-    # (.*?) 是非贪婪匹配,加上? 以最少的可能性匹配 默认是贪婪方式，尽可能多的可能来匹配
-    match_obj = re.match(r'(.*) are (.+?)(.+)', line, re.I)
-
-    if match_obj:
-        print(match_obj.groups())  # 返回一个包含所有小组字符串的元组
-        print(match_obj.group())  # 匹配的整个表达式的字符串结果
-        print(match_obj.group(1))  # 从1开始，匹配对象中的索引
-        print(match_obj.group(2))
-        # print(match_obj.group(3))
-    else:
-        print("match result is None!")
-    '''
-    re.research(pattern, string, flags = 0)
-    
-    输入和输出：类似re.match
-    
-    区别： 可以在任意位置开始匹配，匹配到就返回匹配对象，否则None
-    
-    '''
-    print(re.search('BAI', 'www.baidu.com', re.I).span())  # 从bai开始匹配成功
-
+    test_re_match()
+    test_re_search()
     test_re_sub()
-
-    str1 = 'A23G4HFD567'
-    # (?P<name> Expression) 命名捕获组 可以通过name来使用匹配组的值
-    str_match = re.search(r'(?P<value>\d+)', str1)
-    print("re.search :  ", str_match.group('value'))
 
     # 想把中英文的逗号过滤掉
     str2 = 'Comuna ，Etropole, Sofia'
     print(string_filter(str2))
 
     # 把以_std结尾的字符串替换为空
-
     str3 = "test_for_std"
     print(re.sub(r"_std$", "", str3))
+
+    # 把以_aliasN结尾的替换成空
+    key = "zh_hk_alias"
+    key = re.sub(r'_alias\d*', '', key)
+    print(key)
---
+++ studying/src/17_comprehension.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+import types
+
+from collections import Iterable
+
+'''
+
+列表(list)推导式
+字典(dict)推导式
+集合(set)推导式
+ref: https://www.jianshu.com/p/0a269715a742
+
+'''
+
+
+def test_comprehension_use():
+    '''
+
+    [x for x in data if condition]  # 此处if主要起条件判断作用，data数据中只有满足if条件的才会被留下，最后统一生成为一个数据列表。
+    [exp1 if condition else exp2 for x in data] # 此处if...else主要起赋值作用，当data中的数据满足if条件时将其做exp1处理，否则按照exp2处理，最后统一生成为一个数据列表
+
+    使用()生成generator：将推导式的[]改成()即可得到生成器
+
+    '''
+
+    # 求(x,y),其中x是0-5之间的偶数，y是0-5之间的奇数组成的元祖列表
+
+    li = [(x, y) for x in range(5) if x %
+          2 == 0 for y in range(5) if y % 2 == 1]
+    print(li)
+
+    # 得到生成器
+    tuple_itor = (i for i in range(10) if i % 3 == 0)
+    for elem in tuple_itor:
+        print(elem)
+
+    '''
+    字典推导式：
+    {key:value for key,value in existing_data_structure }: 字典推导和列表推导的使用类似，只不过中括号改成大括号
+    
+    '''
+    # 字典的kv互换
+    di1 = {"a": 10, "b": 11, "c": 13}
+    di2 = {v: k for k, v in di1.items() if k in ['a', 'b']}
+    print(di2)
+
+    '''
+    集合推导式：
+    基本格式：{ expr for value in collection if condition }
+    
+    '''
+    names = [['Tom', 'Billy', 'Jefferson', 'Andrew', 'Wesley', 'Steven', 'Joe'], [
+        'Alice', 'Jill', 'Ana', 'Wendy', 'Jennifer', 'Sherry', 'Eva']]
+
+    # 用嵌套列表实现
+    name_set = {name for lst in names for name in lst if name.count(
+        'e') >= 2}  # 注意遍历顺序，这是实现的关键
+
+    print(name_set, type(name_set))
+
+# ['Jefferson', 'Wesley', 'Steven', 'Jennifer']
+
+
+'''
+yield :  使用
+
+1、函数f中使用yield  -》 generator function
+   f : 生成器函数 
+   f(VAR): 返回生成器对象：iterable对象
+   iterable对象：有next()函数，如果被用在for循环中，next会自动调用
+   
+   程序执行逻辑： 遇到yield, 中断，返回迭代值；下一轮调用，会从中断初开始执行；
+   
+'''
+
+
+def fab(max: int):
+    # 计算斐波那契数列
+    n, a, b = 0, 0, 1
+
+    while n < max:
+        yield b  #
+        a, b = b, a+b
+        n += 1
+
+
+'''
+
+迭代器对象： Iterable
+
+1、普通的可迭代的数据类型：转成迭代器对象
+iter(object[, sentinel])
+参数：
+object - - 支持迭代的集合对象。
+sentinel - - 如果传递了第二个参数，则参数 object 必须是一个可调用的对象（如，函数），此时，iter 创建了一个迭代器对象，每次调用这个迭代器对象的__next__()方法时，都会调用 object。
+打开模式
+返回值 ： 迭代器对象。
+
+2、next()函数：
+
+next(iterable[, default])
+参数说明：
+iterable -- 可迭代对象
+default -- 可选，用于设置在没有下一个元素时返回该默认值，如果不设置，又没有下一个元素则会触发 StopIteration 异常。
+'''
+
+
+def test_iterable():
+    # 普通的可迭代的数据类型：转成迭代器对象
+    lst = [1, 2, 3, 4]
+    for i in iter(lst):  # 自动调用next
+        print(i)
+
+    it = iter([1, 2, 3, 4, 5])
+    while True:
+        try:
+            elem = next(it)
+            print('while iterator:', elem)
+        except StopIteration:
+            #  遇到StopIteration退出循环
+            break
+
+
+if __name__ == '__main__':
+
+    test_comprehension_use()
+
+    # fab判断是否为： 生成器类型？迭代器对象？
+
+    print("fab types: ", isinstance(fab, types.GeneratorType))
+    print("fab(2) types: ", isinstance(fab(2), types.GeneratorType))
+
+    print("fab instance: ", isinstance(fab, Iterable))
+    print("fab(2) instance: ", isinstance(fab(2), Iterable))
+
+    # 测试迭代器对象的使用
+    test_iterable()
