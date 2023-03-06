2e0085092045d7ff08a1cd2c3bcd3ea461f5def1
veger <ww5365@163.com>
2023-03-06 21:54:01 +0800

    Update lesson-02.py

--- deepshare/lesson-02.py
+++ deepshare/lesson-02.py
@@ -101,7 +101,83 @@ Traning-servering skew
 模型膨胀 ： 
 
 '''
+        '''
+    矩阵运算
+
+    torch.mul(input, other)
+    对位乘法： 点乘
     
+    torch.mm(input, mat2)
+    矩阵乘法
+
+    torch.matmul(input, other)
+
+
+    torch.add(input=, other=, alpha = )
+
+    功能：input + other * alpha 
+
+
+
+    '''
+
+    t11 = torch.randint(10, (2, 3))
+
+    t12 = torch.randint(5, (2,3))
+
+    t13 = torch.mul(t11, t12)  # t11, t12的维度必须相同
+
+    print("t11: {} \n t12:{}\n t13:{}".format(t11, t12, t13))
+
+
+    t12_1 = t12.t()
+    t14 = torch.mm(t11, t12_1)  # t11的列，必须和t12的行相同： m * n  n * q => m * q
+
+    print("t11: {} \n t12_1:{}\n t14:{}".format(t11, t12_1, t14))
+
+
+    t15 = torch.ceil(torch.rand(3) * 10)  # 1d 向量： 2个元素
+    t16 = torch.ceil(torch.rand(3) * 10) 
+    t17 = torch.randint(10, size=(1,2))  # 矩阵： 1*2
+
+    t18 = torch.matmul(t15, t16)  # 向量的点积  标量
+
+    print("t15: {} \n t15 size:{}\n t16:{} t16 size:{} t18:{} t18 size:{}".format(t15, t15.shape, t16,  t16.shape, t18, t18.shape))
+
+    t19 = torch.rand(2,4)
+    t20 = torch.rand(4,3) ###维度也要对应才可以乘  矩阵乘法
+    print(torch.matmul(t19,t20),'\n',torch.matmul(t19,t20).size())
+
+
+    t21 = torch.ceil(torch.rand(4) * 10)  # 1d 向量： 4个元素
+    t22 = torch.ceil(torch.rand(4, 3) * 10)   # 2d 矩阵
+    t23 = torch.matmul(t21, t22)
+
+    ### python 中的广播机制，处理一些维度不同的tensor结构进行相乘操作， 1d * 2d 处理过程如下：
+    ### 扩充x =>(,4) 
+    ### 相乘x(,4) * y(4,3) =>(,3) 
+    ### 去掉1D =>(3)
+
+    print("t21: {} \n t21 size:{}\n t22:{} t22 size:{} t23:{} t23 size:{}".format(t21, t21.shape, t22,  t22.shape, t23, t23.shape))
+
+
+    t24 = torch.ceil(torch.rand(4, 3) * 10)  # 2d 矩阵： 4 * 3
+    t25 = torch.ceil(torch.rand(3) * 10)   # 1d 向量 
+    t26 = torch.matmul(t24, t25)  ## 进行点积运算，4个元素的向量
+
+    print("t24: {} \n t24 size:{}\n t25:{} t25 size:{} t26:{} t26 size:{}".format(t24, t24.shape, t25,  t25.shape, t26, t26.shape))
+
+
+
+    t27 = torch.rand((2, 3))
+
+    t28 = torch.rand((2,3))
+
+    t29 = torch.add(t27, t28, alpha= 2)
+
+    
+    print("t27: {} \n t27 size:{}\n t28:{} t28 size:{} t29:{} t29 size:{}".format(t27, t27.shape, t28,  t28.shape, t29, t29.shape))
+
 
 
 
