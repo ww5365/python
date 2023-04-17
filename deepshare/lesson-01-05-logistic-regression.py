3f29d7fd195ee3cdcc742a980bf7c309b167f235
veger <ww5365@163.com>
2023-04-17 15:39:53 +0800

    Create lesson-01-05-logistic-regression.py

---
+++ deepshare/lesson-01-05-logistic-regression.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+import torch
+import numpy as np
+
+import torch.nn as nn
+import matplotlib.pyplot as plt
+
+torch.manual_seed(10)
+
+def test_logistic_regression():
+    '''
+    逻辑回归：
+    
+    机器学习模型训练的步骤：
+
+    数据
+
+    模型
+
+    损失函数
+
+    优化器
+
+    迭代训练
+
+    '''
+
+    #===========样本数据===================
+
+    sample_nums = 200
+    mean_value = 1.7
+    bias = 1
+    n_data = torch.ones(sample_nums, 2)  # 100 * 2 张量 [100, 2]
+
+    x0 = torch.normal(n_data * mean_value, 1) + bias
+    y0 = torch.zeros(sample_nums)  # 类别0的label, 长度为100的向量  [100]
+
+
+    x1 = torch.normal(-mean_value * n_data, 1) + bias  #类别1， [100, 2]
+    y1 = torch.ones(sample_nums)  # 类别1的label, 长度为100的向量  [100]
+
+    train_x = torch.cat([x0, x1], dim = 0)  # 200 * 2
+    train_y = torch.cat([y0, y1], dim = 0) # [100] 向量
+
+    print("n_data tensor: {} trainy: {}".format(train_x, train_y))
+    print("n_data tensor type: {} y0 dtype: {} y0: {}".format(n_data.shape, y0.shape, y0))
+
+
+    #============模型选择========================
+
+    class LR(nn.Module):
+
+        def __init__(self):
+            super(LR, self).__init__()   # super() 是调用父类的方法 python2： super(Class, self) python3: super()
+            self.features = nn.Linear(2, 1)  
+            # 输入特征数为2，输出特征数为1  Y = X * W +  b
+            # 查看模型参数
+            # for param in self.features.parameters():
+            #   print(param)
+
+            self.sigmoid = nn.Sigmoid()
+        
+        def forward(self, x):
+
+            x = self.features(x)
+            x = self.sigmoid(x)
+            return x
+
+    lr_net = LR()    ## 实例化逻辑回归模型
+
+
+    #==================损失函数=======================
+
+    loss_fn = nn.BCELoss()   # 二分类的交叉熵函数
+
+    #==================优化器========================
+
+    lr = 1e-2   # 学习率
+
+    optimizer = torch.optim.SGD(lr_net.parameters(), lr=lr, momentum=0.9)
+
+
+    #================模型训练=========================
+
+    for iteration in range(200):
+
+        # 前向传播
+        y_pred = lr_net(train_x)   #这里对象是自动调用了forward(self, x) ? 返回：20 * 1 结果
+
+        #计算loss
+        loss = loss_fn(y_pred.squeeze(), train_y)
+
+
+        print("y_pred: {}  y_pred shape: {} squeeze: {}".format(y_pred, y_pred.shape, y_pred.squeeze()))
+
+        # 反向传播
+
+        loss.backward()
+
+        # 更新参数
+        optimizer.step()
+
+
+        # 绘图看效果
+        if  iteration % 20 == 0:
+            mask = y_pred.ge(0.5).float().squeeze()  # 以0.5为阈值进行分类, 大于0.5的变成1.(浮点数),同时降维 100 * 1 -》 100
+
+            correct = (mask == train_y).sum()  # 张量中，对位元素相同的个数统计;torch.int64 的标量;预测为1/0且实际为1/0的个数,也就是预测正确的个数
+
+            acc = correct.item()/train_y.size(0) # 计算准确率   
+            # tensor.item() 返回一个数，仅适用张量只有1个元素的情况
+            # Tensor.size(dim=None) → torch.Size or int  指明dim，返回特定维度值； 否则，返回的tuple
+
+            plt.scatter(x0.data.numpy()[:, 0],x0.data.numpy()[:, 1], c = 'r', label = "class 0") # 两个维度特征(x1, x2),标注到xy坐标系上
+            plt.scatter(x1.data.numpy()[:, 0],x1.data.numpy()[:, 1], c = 'b', label = "class 1")
+
+            print("==" * 40)
+
+            print("self.features: {} type:{}".format(lr_net.features.weight, lr_net.features.weight.shape))  # weight: 1 * 2
+            print("self.features: {} type:{}".format(lr_net.features.weight[0], lr_net.features.weight[0].shape))
+
+            print("self.features: {} type:{} bias:{}".format(lr_net.features.bias, lr_net.features.bias.shape, lr_net.features.bias[0]))
+            
+            print("==" * 40)
+
+
+            w0, w1 = lr_net.features.weight[0]
+
+            w0, w1 = float(w0.item()), float(w1.item())
+
+            plot_b = float(lr_net.features.bias[0].item())
+
+            plot_x = np.arange(-6, 6, 0.1)  # range 和 arange 区别： 都不包含end，arrange步长支持小数
+
+            plot_y = (-w0 * plot_x - plot_b) / w1  ## 这条线，画出来？ 是什么原理？
+
+            # 画线参考：https://blog.csdn.net/weixin_43772533/article/details/100974008
+
+
+            plt.xlim(-5, 7)
+            plt.ylim(-7, 7)
+            plt.plot(plot_x, plot_y)
+
+            plt.text(-5, 5, 'loss: %.4f' % loss.data.numpy(), fontdict={'size': 20, 'color': 'red'})
+
+            plt.title("iteration: {}\n w0:{:.2f}, w1:{:.2f}, b:{:.2f}, acc:{:.2f}".format(iteration, w0, w1, plot_b, acc))
+
+            plt.legend()
+            plt.show()
+            # plt.pause(0.5)
+
+            if acc > 0.99:
+                break
+            
+
+def lesson01_05():
+
+    test_logistic_regression()
+
+    return
+
+    
+
+if __name__ == '__main__':
+
+    lesson01_05()
+    
+    y = torch.tensor(
+    [[0.3043],
+        [0.2677],
+        [0.1732],
+        [0.2540]]
+    )
+
+    print("y shape:{} type: {}".format(y.shape, y.dtype))
+
+    print("y shape:{} ".format(y.ge(0.2).float().squeeze()))
+
+    x1 = torch.tensor([1.2, 3.2, 1.0])
+    x2 = torch.tensor([1.3, 3.2, 1.0])
+
+    sum = (x1==x2).sum()   
+
+    print("x1 == x2: {}  sum: {} type:{}".format(x1.size(), (x1==x2).sum(), sum.dtype))
+
