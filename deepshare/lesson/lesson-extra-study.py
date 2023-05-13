c8081596036e430acdd599585c0834a1f30012b5
veger <ww5365@163.com>
2023-05-13 22:51:05 +0800

    Update lesson-extra-study.py

--- deepshare/lesson/lesson-extra-study.py
+++ deepshare/lesson/lesson-extra-study.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           # -*- coding: utf-8 -*-
 
 import torch
 import numpy as np
@@ -9,6 +9,13 @@ import random  # 使用random.shuffle(x)
 from torch.utils.data import IterableDataset
 
 from torch.utils.data import DataLoader
+
+
+import torch.nn as nn
+
+torch.manual_seed(1)
+
+# random.seed(1)
 
 
 def lesson02_extra():
@@ -67,16 +74,59 @@ def lesson02_extra():
 
     dl = DataLoader(MyDataset(), batch_size = 5)
 
-    # 可迭代对象在初始化中会调用一次​​__init__​​​方法，在获取迭代器时会调用一次​​__iter__​​​方法，之后在获取元素时，每获取一个元素都会调用一次​​__next__​​方法
-    
+    # 可迭代对象在初始化中会调用一次​​__init__​​​方法
+    # 在获取迭代器时会调用一次​​__iter__​​​方法，之后在获取元素时，每获取一个元素都会调用一次​​__next__​​方法
+
     for i, data in enumerate(dl):
         print(i, data)
 
-    
+    d1 = random.uniform(0, 1)
+
+    mask = np.random.choice([0,1,2], size= (2,3,1), p=[0.9, 0.05, 0.05])
+    mask2 = np.repeat(mask, 3, axis=2)
+
+    print("d1 type: {}".format(d1))
+    print("mask type: {}".format(mask))
+    print("mask2 type: {}".format(mask2))
+
+def lesson03_extra():
+
+    '''
+    nn.Linear  全连接网络使用
+
+    参考：
+    https://blog.csdn.net/zhaohongfei_358/article/details/122797190
+    https://zhuanlan.zhihu.com/p/557253923
+
+    '''
+
+    input = torch.randn(10, 3)  # 10个样本，每个样本3个特征
+
+    print("input: {} dtype:{} \n".format(input, input.dtype))
+
+    model = nn.Linear(3, 7)  # 3个特征(神经元)， 输出7个特征
+    output = model(input)
+
+    for para in model.parameters():
+        print("modle parameters: {} \n".format(para)) 
+        # 有两个参数：W ： 7 * 3 tensor 运算时转置
+        # bias：1 * 7 
+
+    print("output:{} \n".format(output))
+    print(output.shape)
+
 
 
 if __name__ == '__main__':
 
-    lesson02_extra()
+    # lesson02_extra()
+    lesson03_extra()
+
+    t1 = torch.tensor([-1.5256, -0.7502, -0.6540])
+    t2 = torch.tensor([-0.3065,  0.1698, -0.1667])
+
+    t3 = torch.matmul(t1, t2)
+
+    print("t3: {}".format(t3 + 0.0843))
 
 
