3f302a199d2ccaeceb0df8e689bb3fdca17a5ee8
veger <ww5365@163.com>
2023-05-13 22:43:43 +0800

    Update lesson-02-03-transforms_methods.py

--- deepshare/lesson/lesson-02-03-transforms_methods.py
+++ deepshare/lesson/lesson-02-03-transforms_methods.py
@@ -36,6 +36,72 @@ log_interval = 10
 val_interval = 1
 rmb_label = {"1": 0, "100": 1}
 
+
+class AddPepperNoise(object):
+    """增加椒盐噪声
+    Args:
+        snr （float）: Signal Noise Rate
+        p (float): 概率值，依概率执行该操作
+
+    椒盐噪声：脉冲噪声  是一种随机出现的白点或黑点
+    信噪比：SNR  signal-noise rate  噪声元素占图片元素的比列
+
+    """
+
+    def __init__(self, snr, p=0.9):
+        assert isinstance(snr, float) and (isinstance(p, float))    # 2020 07 26 or --> and
+        self.snr = snr
+        self.p = p
+
+    def __call__(self, img):
+        """
+        Args:
+            img (PIL Image): PIL Image
+        Returns:
+            PIL Image: PIL image.
+        """
+        if random.uniform(0, 1) < self.p:
+            img_ = np.array(img).copy()
+            h, w, c = img_.shape
+            signal_pct = self.snr
+            noise_pct = (1 - self.snr)
+            mask = np.random.choice((0, 1, 2), size=(h, w, 1), p=[signal_pct, noise_pct/2., noise_pct/2.]) # 0 原图像 90%  1 盐噪声
+
+            '''
+            choice(a, size=None, replace=True, p=None)
+            a: 给定的一维数组且必须是一维数组。可以有两种给值，给定一个数组，例如a=[1,2,3,4]
+               给定一个正值，此时就相当于从列表np.arange(a)中随机抽取D个整数
+            size: 表示想要得到的数的个数，
+                  如果size=3,表示得到3个数。
+                  如果size=(3,2)，表示得到3*2个数，且形状为3*2
+            replace: 表示是否重复选取，默认表示可以重复选择.
+            p: 表示列表中某数被选取的概率，默认为正态分布
+            '''
+
+            mask = np.repeat(mask, c, axis=2)
+
+            '''
+            np.repeat(a, repeats, axis=None)
+            对a数组中的对应元素进行重复复制
+        
+            a=np.array([[10,20],[30,40]])  
+            np.repeat(a, [3,2], axis = 0)
+
+            [10,20]
+            [10,20]
+            [10,20]
+            [30,40]
+            [30,40]
+
+            '''
+
+            img_[mask == 1] = 255   # 盐噪声 白色
+            img_[mask == 2] = 0     # 椒噪声 黑色
+            return Image.fromarray(img_.astype('uint8')).convert('RGB')
+        else:
+            return img
+
+
 # ============================ step 1/5 数据 ============================
 split_dir = os.path.abspath(os.path.join(BASE_DIR, "..", "data", "rmb_split"))
 if not os.path.exists(split_dir):
@@ -99,9 +165,9 @@ train_transform = transforms.Compose([
     # transforms.ColorJitter(hue=0.3),
 
     # 3 Grayscale  灰度图
-    transforms.Grayscale(num_output_channels=3),
+    # transforms.Grayscale(num_output_channels=3),
 
-    # 4 Affine  仿射变换
+    # 4 Affine  仿射变换  旋转 平移 缩放 错切 翻转
     # transforms.RandomAffine(degrees=30),
     # transforms.RandomAffine(degrees=0, translate=(0.2, 0.2), fillcolor=(255, 0, 0)),
     # transforms.RandomAffine(degrees=0, scale=(0.7, 0.7)),
@@ -113,16 +179,38 @@ train_transform = transforms.Compose([
     # transforms.RandomErasing(p=1, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=(254/255, 0, 0)),
     # transforms.RandomErasing(p=1, scale=(0.02, 0.33), ratio=(0.3, 3.3), value='1234'),
 
-    # 1 RandomChoice
+    # 1 RandomChoice  一组transforms操作中，随机选择
     # transforms.RandomChoice([transforms.RandomVerticalFlip(p=1), transforms.RandomHorizontalFlip(p=1)]),
 
-    # 2 RandomApply
+    # 2 RandomApply  执行一组transforms操作
     # transforms.RandomApply([transforms.RandomAffine(degrees=0, shear=45, fillcolor=(255, 0, 0)),
     #                         transforms.Grayscale(num_output_channels=3)], p=0.5),
-    # 3 RandomOrder
+    # 3 RandomOrder  对一组transforms操作打乱顺序
     # transforms.RandomOrder([transforms.RandomRotation(15),
     #                         transforms.Pad(padding=32),
     #                         transforms.RandomAffine(degrees=0, translate=(0.01, 0.1), scale=(0.9, 1.1))]),
+
+    # 自定义transform 
+    # 1: 仅接收一个参数，返回一个参数  2：注意上下游的输入和输出
+    
+    # 
+    # class Compose(object):
+    #    def __call__(self, img):
+    #        for t in self.transforms:
+    #            img = t(img)
+    #            return img
+
+    # 通过类实现多个参数传入：
+    # class  YourTransforms(object):
+    #    def __init__(self, ...):
+    #        ...
+    #    def __call__(self, img):
+    #        ...
+    #        return img
+
+
+    # eg：增加椒盐噪声
+    AddPepperNoise(0.9, p=0.5),
 
     transforms.ToTensor(),
     transforms.Normalize(norm_mean, norm_std),
@@ -131,7 +219,7 @@ train_transform = transforms.Compose([
 valid_transform = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.Normalize(norm_mean, norm_std)
+    transforms.Normalize(norm_mean, norm_std) # 数据标准化处理
 ])
 
 # 构建MyDataset实例
