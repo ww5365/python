4c394218d1fd8cb41fc98da6cedb22495bbcafd1
veger <ww5365@163.com>
2023-05-22 10:16:43 +0800

    Update lesson-03-03-nn_layers_convolution.py

--- deepshare/lesson/lesson-03-03-nn_layers_convolution.py
+++ deepshare/lesson/lesson-03-03-nn_layers_convolution.py
@@ -20,7 +20,7 @@ sys.path.append(hello_pytorch_DIR)
 
 from tools.common_tools import transform_invert, set_seed
 
-set_seed(3)  # 设置随机种子
+set_seed(2)  # 设置随机种子
 
 # ================================= load img ==================================
 path_img = os.path.join(os.path.dirname(os.path.abspath(__file__)), "lena.png")
@@ -54,7 +54,6 @@ dilation: 空洞卷积
 groups： 分组卷积设置
 bias： 偏置
 
-
 '''
 
 flag = 1
@@ -64,27 +63,38 @@ if flag:
     nn.init.xavier_normal_(conv_layer.weight.data)
 
     # calculation
-    img_conv = conv_layer(img_tensor)
+    print("tensor shape: {}".format(img_tensor.shape))
+    img_conv = conv_layer(img_tensor)  # 1 * 1 * 510 * 510
+    print("conv tensor shape: {}".format(img_conv.shape))
 
 # ================ transposed
 # flag = 1
-flag = 0
+flag = 0   
+
 if flag:
     conv_layer = nn.ConvTranspose2d(3, 1, 3, stride=2)   # input:(i, o, size)
     nn.init.xavier_normal_(conv_layer.weight.data)
 
     # calculation
-    img_conv = conv_layer(img_tensor)
-
+    img_conv = conv_layer(img_tensor)   # 1 * 1 * 1025 * 1025 没有明白为什么？ stride=2  扩展了img_conv的维度到1025
 
 # ================================= visualization ==================================
 print("卷积前尺寸:{}\n卷积后尺寸:{}".format(img_tensor.shape, img_conv.shape))
-img_conv = transform_invert(img_conv[0, 0:1, ...], img_transform)
+
+'''
+Tensor的任意维度上进行切片操作，PyTorch已经封装好了两个运算符:和...，它们的用法如下：
+:常用于对一个维度进行操作，基本的语法形式是：start:end:step。单独使用:代表全选这个维度，start和end为空分别表示从头开始和一直到结束，step的默认值是1。
+...用于省略任意多个维度，可以用在切片的中间，也可以用在首尾。
+'''
+
+t = img_conv[0, 0:1, ...]
+
+print("t shape: {}".format(t.shape))
+
+img_conv = transform_invert(img_conv[0, 0:1, ...], img_transform)  # 第0 维度取0行索引  第1维度取0~1行，剩下维度取任意多个维度
+
 img_raw = transform_invert(img_tensor.squeeze(), img_transform)
 
 plt.subplot(122).imshow(img_conv, cmap='gray')
 plt.subplot(121).imshow(img_raw)
 plt.show()
-
-
-
